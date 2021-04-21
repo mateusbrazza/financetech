4a96ae998803da7cca5a1a2ede933d06136580a6
Mateus Santana <mateusjoaquim@live.com>
2021-04-21 17:22:12 -0300

    inicializer project

---
+++ src/main/java/com/financetechbackend/FinancetechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.financetechbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancetechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancetechBackendApplication.class, args);
+	}
+
+}
